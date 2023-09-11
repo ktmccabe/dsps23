51758c13c7234c8db895a955d7abd5e1443bf92d
ktmccabe <kt2mccabe@gmail.com>
2023-09-11 09:52:00 -0400

    updates to description and causality read_csv function

---
+++ practiceweek1.R
@@ -0,0 +1,14 @@
+## Problem Set 1
+## your name
+
+
+## Problem 1.1
+
+## setting my working directory
+
+setwd("/Users/ktmccabe/Dropbox/GitHub2/dsps23")
+
+## Problem 1.2
+
+sumprob11 <- 8 + 4 - 5
+sumprob11
