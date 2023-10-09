4f79bd7166aa0dbe7d319cede815be996ed10b27
ktmccabe <kt2mccabe@gmail.com>
2023-10-09 10:32:25 -0400

    add loop section

--- justiceprep.R
+++ justiceprep.R
@@ -4,6 +4,11 @@ justices <- read.csv("data/justicesfull.csv")
 justices <- subset(justices, term >= 1991 & term != 2005)
 write.csv(justices, "data/justices20.csv")
 
+## 2023 update
+justices <- read.csv("data/justices2023.csv")
+justices <- subset(justices, term >= 1991 & term != 2005)
+write.csv(justices, "data/justices23.csv")
+
 
 justices$justiceName <- as.character(justices$justiceName)
 medians <- tapply(justices$post_mn, justices$term, median)
